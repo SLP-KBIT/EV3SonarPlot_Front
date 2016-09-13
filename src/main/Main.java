a0d0792fa1ece6567054a84683b60dd92d2e04a0
kishila <s16g459@stu.kagawa-u.ac.jp>
2016-09-13 15:14:08 +0900

    スケジュール作成

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,65 @@
 package main;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import lejos.hardware.lcd.LCD;
+import task.DriveTask;
+
 public class Main {
 
+	// スケジューラ
+	private ScheduledExecutorService scheduler;
+    private ScheduledFuture<?> futureDrive;
+
+    // タスク
+    private DriveTask  driveTask;
+
+    /**
+     * コンストラクタ。
+     * スケジューラとタスクオブジェクトを作成。
+     */
+    public Main() {
+    	scheduler  = Executors.newScheduledThreadPool(1);
+    	driveTask  = new DriveTask();
+    }
+
+    /**
+     * 走行開始時の作業スケジューリング。
+     */
+    public void start() {
+        futureDrive = scheduler.scheduleAtFixedRate(driveTask, 0, 5, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * 走行終了時のタスク終了後処理。
+     */
+    public void stop () {
+        if (futureDrive != null) {
+            futureDrive.cancel(true);
+        }
+    }
+
+    /**
+     * スケジューラのシャットダウン。
+     */
+    public void shutdown() {
+        scheduler.shutdownNow();
+    }
+
+    /**
+     * メイン
+     */
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		LCD.drawString("Please Wait...  ", 0, 4);
 
+		Main main = new Main();
+
+		LCD.drawString("Running       ", 0, 4);
+
+        main.stop();
+        main.shutdown();
 	}
-
 }
---
+++ src/task/DriveTask.java
@@ -0,0 +1,12 @@
+package task;
+
+public class DriveTask implements Runnable {
+
+    /**
+     * EV3本体の制御。
+     */
+    @Override
+    public void run() {
+
+    }
+}
